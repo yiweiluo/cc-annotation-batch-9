30d83a750dfaebdf12d0c86a3e46267cc127dda3
yiweiluo <yiweiluo@gmail.com>
2020-01-10 16:31:56 -0800

    live

--- config/config_deploy.js
+++ config/config_deploy.js
@@ -12,7 +12,7 @@ var config_deploy = {
     // set deployment method; use one of:
     //'debug', 'localServer', 'MTurk',
     // 'MTurkSandbox', 'Prolific', 'directLink'
-    "deployMethod": "MTurkSandbox",
+    "deployMethod": "MTurk",
 
     // who to contact in case of trouble
     "contact_email": "yiweil@stanford.edu",
